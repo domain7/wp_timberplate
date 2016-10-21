d268e6cbd15e8bc7f4a0f6441abe42d89a35dcfe
Jamie Graham <jamie@domain7.com>
2016-10-21 12:08:34 -0700

    Added menu and sidebar includes, changed screenshot

--- functions.php
+++ functions.php
@@ -6,3 +6,5 @@ include_once 'includes/theme_setup.php'; // Theme Setup and Clean up
 include_once 'includes/assets.php'; // Enque Styles and Scripts
 include_once 'includes/image_sizes.php'; // image size definitions
 include_once 'includes/login_page.php'; // Customize admin login
+include_once 'includes/sidebars.php'; // Register sidebar positions
+include_once 'includes/menus.php'; // Register menu positions
